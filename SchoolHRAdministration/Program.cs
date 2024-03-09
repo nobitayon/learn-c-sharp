4fbf1d1638bf0175813984cac69c1ec444482f42
Yonathan Manurung <yonrungrung@gmail.com>
2024-03-09 09:40:17 +0700

    abstracting object instantiate process of create Employee

--- SchoolHRAdministration/Program.cs
+++ SchoolHRAdministration/Program.cs
@@ -2,7 +2,16 @@
 
 namespace SchoolHRAdministration
 {
+    public enum EmployeeType
+    {
+        Teacher,
+        HeadOfDepartment,
+        DeputyHeadMaster,
+        HeadMaster
+    }
     class Program
+
+
     {
         static void Main(string[] args)
         {
@@ -13,66 +22,70 @@ namespace SchoolHRAdministration
             {
                 totalSalaries += employee.Salary;
             }
-            Console.WriteLine($"Total Annual Salaries (including bonus): {totalSalaries}");
+            // Console.WriteLine($"Total Annual Salaries (including bonus): {totalSalaries}");
+
+
+            Console.WriteLine($"(using linq)Total Annual Salaries (including bonus): {employees.Sum(e => e.Salary)}");
 
             Console.ReadKey();
 
         }
 
-        public static void SeedData(List<IEmployee> employees) {
+        public static void SeedData(List<IEmployee> employees)
+        {
 
-            IEmployee teacher1 = new Teacher
-            {
-                Id = 1,
-                FirstName="Bob",
-                LastName = "Fisher",
-                Salary = 40000
 
-            };
+            IEmployee teacher1 = EmployeeFactory.GetEmployeeInstance(
+                EmployeeType.Teacher,
+                1,
+                "Bob",
+                "Fisher",
+                40000
+            );
 
             employees.Add(teacher1);
 
-            IEmployee teacher2 = new Teacher
-            {
-                Id = 1,
-                FirstName = "Jenny",
-                LastName = "Thomas",
-                Salary = 40000
+            IEmployee teacher2 = EmployeeFactory.GetEmployeeInstance(
+                EmployeeType.Teacher,
+                1,
+                "Jenny",
+                "Thomas",
+                40000
+            );
 
-            };
 
             employees.Add(teacher2);
 
-            IEmployee headOfDepartment = new HeadOfDepartment
-            {
-                Id = 3,
-                FirstName = "Brenda",
-                LastName = "Mullins",
-                Salary = 50000
+            IEmployee headOfDepartment = EmployeeFactory.GetEmployeeInstance(
+                EmployeeType.HeadOfDepartment,
+                3,
+                "Brenda",
+                "Mullins",
+                50000
+            );
 
-            };
 
             employees.Add(headOfDepartment);
 
-            IEmployee deputyHeadMaster = new DeputyHeadMaster
-            {
-                Id = 4,
-                FirstName = "Devlin",
-                LastName = "Brown",
-                Salary = 60000
+            IEmployee deputyHeadMaster = EmployeeFactory.GetEmployeeInstance(
+                EmployeeType.DeputyHeadMaster,
+                4,
+                "Devlin",
+                "Brown",
+                60000
+                );
 
-            };
 
             employees.Add(deputyHeadMaster);
 
-            IEmployee headMaster = new HeadMaster
-            {
-                Id = 5,
-                FirstName = "Damien",
-                LastName = "Jones",
-                Salary = 80000
-
-            };
+            IEmployee headMaster = EmployeeFactory.GetEmployeeInstance(
+                EmployeeType.HeadMaster,
+                5,
+                "Damien",
+                "Jones",
+                80000
+                );
+                
 
             employees.Add(headMaster);
         }
@@ -80,7 +93,7 @@ namespace SchoolHRAdministration
 
     public class Teacher : EmployeeBase
     {
-        public override decimal Salary { get => base.Salary + (base.Salary*0.02m); }
+        public override decimal Salary { get => base.Salary + (base.Salary * 0.02m); }
     }
 
     public class HeadOfDepartment : EmployeeBase
@@ -94,9 +107,35 @@ namespace SchoolHRAdministration
         public override decimal Salary { get => base.Salary + (base.Salary * 0.04m); }
     }
 
-    public class HeadMaster: EmployeeBase
+    public class HeadMaster : EmployeeBase
     {
         public override decimal Salary { get => base.Salary + (base.Salary * 0.05m); }
+    }
+
+    public static class EmployeeFactory
+    {
+        public static IEmployee GetEmployeeInstance(EmployeeType employeeType, int id, string firstName, string lastName, decimal salary)
+        {
+            IEmployee employee = null;
+            switch (employeeType)
+            {
+                case EmployeeType.Teacher:
+                    employee = new Teacher { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+                    break;
+                case EmployeeType.HeadOfDepartment:
+                    employee = new Teacher { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+                    break;
+                case EmployeeType.DeputyHeadMaster:
+                    employee = new DeputyHeadMaster { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+                    break;
+                case EmployeeType.HeadMaster:
+                    employee = new HeadMaster { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+                    break;
+                default:
+                    break;
+            }
+            return employee;
+        }
     }
 
 
